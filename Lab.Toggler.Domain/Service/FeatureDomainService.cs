d92331936b1195b728cea0c107900f985d53859e
Fernando Batista <fbatist@hotmail.com>
2018-09-23 01:14:25 -0300

    feature domain service

---
+++ Lab.Toggler.Domain/Service/FeatureDomainService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace Lab.Toggler.Domain.Service
+{
+    public class FeatureDomainService : ServiceBase
+    {
+        public FeatureDomainService(IMediator mediator) : base(mediator)
+        {
+        }
+
+
+    }
+}
---
+++ Lab.Toggler.Tests/Unit/Domain/Service/FeatureDomainServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab.Toggler.Tests.Unit.Domain.Service
+{
+    public class FeatureDomainServiceTest
+    {
+
+    }
+}
